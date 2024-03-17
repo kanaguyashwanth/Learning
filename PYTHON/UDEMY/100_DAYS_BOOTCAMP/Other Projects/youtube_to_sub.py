1301c9465e0bfa9e4ee8b89db2222d21723bd92d
Yashwanth K <kanaguyashwanth@gmail.com>
2024-03-17 09:52:49 +0530

    17/03/2024

---
+++ PYTHON/UDEMY/100_DAYS_BOOTCAMP/Other Projects/youtube_to_sub.py
@@ -0,0 +1,21 @@
+import os
+import moviepy.editor as mp
+import speech_recognition as sr
+from pytube import YouTube
+
+# Download YouTube video
+url = 'https://www.youtube.com/watch?v=yx3eR04wdfY&list=PL6X5MDnC9oqOrCtwCxo3VsVMbLpNW7SkK&index=2'
+yt = YouTube(url)
+yt.streams.filter(only_audio=True).first().download(output_path='.', filename='audio')
+
+# Convert video to audio
+clip = mp.VideoFileClip('audio.mp4')
+clip.audio.write_audiofile('audio.wav')
+
+# Perform speech recognition
+recognizer = sr.Recognizer()
+with sr.AudioFile('audio.wav') as source:
+    audio_text = recognizer.recognize_google(audio=source)
+
+# Output the extracted text
+print(audio_text)
